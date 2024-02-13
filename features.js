5b281044acc4f9a1b3a965420bab0f9ccd1b6561
Ashmit Shukla <ashmitshukla18@gmail.com>
2024-02-14 01:41:52 +0530

    NodeJS basics

---
+++ features.js
@@ -0,0 +1,10 @@
+// const gName = "MrsRandom"
+// export const bName = "MrRandom"
+// export default gName
+
+export const percentLove = () => {
+      return `${Math.floor(Math.random()*100)}%`;
+
+      // //Another way to write
+      // return `${~~(Math.random()*100)}%`;
+}
--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-const http = require("http");
+import http from "http"
+import { percentLove } from "./features.js";
+import fs from "fs"//with the help of fs we can read any file
+import path from "path"
+
+console.log(path.extname("/home.random/index.html"));//extension name bata deta hai jaise .html , .js
+
+// const home = fs.readFileSync("./index.html", ()=>{
+//       console.log("File read successfully");
+// })
+
+// console.log(home);
+
+//console.log(percentLove());
+
+// console.log(myObj);
+
 //console.log(http);
+// console.log(myObj.gName);
+
+const home = fs.readFileSync("./index.html");//Synchronously jab tak yeh file na ho read tab tak aage na badho
+
+console.log(home);
 
 const server = http.createServer((req, res) => {
       //console.log(req.url);
@@ -9,7 +30,7 @@ const server = http.createServer((req, res) => {
 
       if(req.url === "/about")
       {
-            res.end("<h1>About</h1>");
+            res.end(`<h1>Love is ${~~(Math.random()*100)}%</h1>`);
       }
       if(req.url === "/contact")
       {
@@ -17,7 +38,9 @@ const server = http.createServer((req, res) => {
       }
       if(req.url === "/")
       {
-            res.end("<h1>Welcome Home</h1>");
+            // fs.readFile("./index.html", (err, home)=>{
+            //       res.end(home);
+            res.end(home);
       }
       else
       {
